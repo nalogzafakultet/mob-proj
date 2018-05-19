f6c8ec7de0a82f0d0c0d9b91ad9f936c07c8a68f
nemsekularac <nsekularac15@raf.edu.rs>
2018-05-19 23:54:53 +0200

    add auth mongo

--- test_app/kumodraz/utils/db.py
+++ test_app/kumodraz/utils/db.py
@@ -1,9 +1,12 @@
 from pymongo import MongoClient
 from .config import *
+from instance.params import *
 
 client = MongoClient(
     host=DB_HOST,
-    port=DB_PORT
+    port=DB_PORT,
+    username=DB_USERNAME,
+    password=DB_SECRET
 )
 
 db = client[DB_DATABASE_NAME]
